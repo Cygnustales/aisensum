a598f280b149c4e222778ed85c0b524fddf09606
Lk6Eng05 <bdkusuma@gmail.com>
2018-04-15 22:11:58 +0700

    update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-dashboard',
@@ -11,4 +12,11 @@ export class DashboardComponent {
   types = ['Jakarta'];
   areai = 'Month';
   areas = ['Jan 2018', 'Feb 2018', 'March 2018', 'April 2018', 'May 2018', 'June 2018'];
+
+  constructor(private routes:Router) {
+
+  }
+  goAway(){
+    this.routes.navigateByUrl('/pages/maps/gmaps')
+  }
 }
